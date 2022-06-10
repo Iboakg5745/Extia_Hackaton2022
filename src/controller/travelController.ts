a65139a5cb85b40ee4d7182f4f15eb723c044106
Mathieu <mathieu.houlle@epitech.eu>
2022-06-10 23:21:45 +0300

    Adding redis

--- src/controller/travelController.ts
+++ src/controller/travelController.ts
@@ -1,11 +1,22 @@
 import express from 'express';
 import { Request } from 'node-fetch';
 import fetch from 'node-fetch';
-
+import { createClient } from 'redis';
 export const TravelController = express.Router();
 
 async function getWeather(city : String)
 {
+
+	const client = createClient();
+	client.on('error', (err : any) => console.log('Redis Client Error', err));
+	await client.connect();
+
+	const value = await client.get('weather-' + city);
+	if (value) {
+		console.log('weather-' + city + " from cache.");
+		console.log(value);
+		return JSON.parse(value);
+	}
 	let res = {"weather": null};
 	await fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=a359a042fe918e1ad8eb21795b23e814&units=metric",
 	{
@@ -23,6 +34,9 @@ async function getWeather(city : String)
 	.then(async data => {
 		let resp = await data.json();
 		res = {"weather": resp.main};
+		await client.set('weather-' + city, JSON.stringify(res));
+		await client.expire('weather-' + city, 60 * 60);
+		console.log('weather-' + city + " from API Request.");
 	}).catch(err => console.error(err));
 	return res;
 }
